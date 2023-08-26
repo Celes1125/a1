31ca8033b656386f5e43549b0f4aba49544d8941
cc <colauttiadad@gmail.com>
2023-08-26 20:41:17 +0200

    organizing directories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RegistroComponent } from './registro/registro.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ConcatenarPipe } from './concatenar.pipe';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { MenuComponent } from './menu/menu.component';
-import { FooterComponent } from './footer/footer.component'
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { FooterComponent } from './components/footer/footer.component'
 
 @NgModule({
   declarations: [
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,18 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-
-
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductosService {
-  
+export class ProductosService {  
   constructor( private http:HttpClient) {    
     
   }
-
     getAll(){
       return this.http.get("https://jsonplaceholder.typicode.com/posts")
       
