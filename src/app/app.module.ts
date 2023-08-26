671d98ff6454fd30f7c6988e80e8937f05c03bab
cc <colauttiadad@gmail.com>
2023-08-26 19:33:05 +0200

    using routerLink, adding a menu and a footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,20 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RegistroComponent } from './registro/registro.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ConcatenarPipe } from './concatenar.pipe';
-import { HomeComponent } from './home/home.component'
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     RegistroComponent,
     ConcatenarPipe,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
