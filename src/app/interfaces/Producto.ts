ce38de456e688a5c2934804f1aa4ebf29c9dbb94
cc <colauttiadad@gmail.com>
2023-08-28 22:34:34 +0200

    adding interfaces and local modules examples

---
+++ src/app/interfaces/Producto.ts
@@ -0,0 +1,16 @@
+export interface Producto {
+    idMeal:string;
+    strMeal:string;
+    strInstructions: string;
+    strMealThumb: string
+}
+
+/* 
+Cuando vienen datos con estructura dinámica>>>
+
+categoria: [
+    {
+        [k: string] = string
+    }
+]
+*/
